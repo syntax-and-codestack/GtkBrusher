9335f3db91a50e2cacab8494381404917d825072
syntax-and-codestack <164366136+syntax-and-codestack@users.noreply.github.com>
2025-02-20 19:26:31 +0000

    filter.cpp

--- gtkbrusher/filter.cpp
+++ gtkbrusher/filter.cpp
@@ -2,6 +2,9 @@
 #include "brush.h"
 #include "Filter.h"
 #include "stdafx.h"
+#include "quake_entity.h"
+#include "eclass.h"
+#include "texture.h"
 
 filter * filter_ptr;
 int f_pFilterId = 0;
@@ -17,30 +20,69 @@ int f_pFilterId = 0;
 #define FILTER_CAULK 005
 #define FILTER_NODRAW 006
 #define FILTER_PORTAL 007
-#define FILTER_TRIGGER 008
-#define FILTER_VOLUME 009
-#define FILTER_LIGHTGRID 010
-#define FILTER_SKYBOX 011
-#define FILTER_LADDER 012
-#define FILTER_SKIP 013
-#define FILTER_HINT 014
-#define CLIP_TERRIAN 015
+#define FILTER_TRIGGER 0x0
+#define FILTER_VOLUME 0X1
+#define FILTER_LIGHTGRID 0X2
+#define FILTER_SKYBOX 0X3
+#define FILTER_LADDER 0X4
+#define FILTER_SKIP 0X5
+#define FILTER_HINT 0X6
+#define FILTER_CLIP_TERRIAN 0X7
 
-#define FILTER_MODEL 016
+#define FILTER_MODEL 0X8
 
-#define FILTER_CHCKBOX 017
+#define FILTER_CHCKBOX 0X9
+
+#define FILTER_ASSERT_COMMAND "F"
 
 filter ** filter_prev(){ filter * f = filter_ptr--; };
 filter ** filter_next(){ filter * f = filter_ptr++; };
 filter ** filter_current(){ filter * f = filter_ptr; };
 
-//filter brush_t
+_QE_BrusherGUI_Table_ * pFilter;
+GtkDialog * FilterDlg;
+GtkCheckButton * FilterBox;
+
+int BrushFilter_Flags = FILTER_BRUSH;
+int EntityFilter_Flags = FILTER_ENTITY;
+int ClipFilter_Flags = FILTER_CLIP;
+int ClipMonsterFilter_Flags = FILTER_CLIP_MONSTER;
+int ClipPlayerFilter_Flags = FILTER_CLIP_PLAYER;
+int CaulkFilter_Flags = FILTER_CAULK;
+int NoDrawFilter_Flags = FILTER_NODRAW;
+int PortalFilter_Flags = FILTER_PORTAL;
+int TriggerFilter_Flags = FILTER_TRIGGER;
+int VolumeFilter_Flags = FILTER_VOLUME;
+int LightGridFilter_Flags = FILTER_LIGHTGRID;
+int SkyBoxFilter_Flags = FILTER_SKYBOX;
+int LadderFilter_Flags = FILTER_LADDER;
+int SkipFilter_Flags = FILTER_SKIP;
+int HintFilter_Flags = FILTER_HINT;
+int ClipTerrianFilter_Flags = FILTER_CLIP_TERRIAN;
+
+//filter brush_t * b
 qboolean FilterBrushClient(brush_t * b){
- brush_t * brush( brush_t*clientsnapf );
- reinterpret_cast<brush_t*>( b = brush( b ));
+brush_t * brush( brush_t*clientsnapf );
+reinterpret_cast<brush_t*>( b = brush( b ));
   if( FilterBrushClient( b ) ){
-    b == NULL && FilterBrushClient( b );
+    b == NULL && brush( b )->g_nBrushId == FILTER_BRUSH && BrushSize() == NULL;
   }
+return true;
 };
+
+//filter quake entity
+qboolean FilterQuakeEntity(quake_entity_t * quake){
+  if( quake->quakentid == FILTER_ENTITY && FilterQuakeEntity( quake )){
+    quake == NULL && quake->EntitySize() == NULL;
+  }
+ return true;
+};
+
+void ConstructBFilterDlg(){
+ if( FILTER_ASSERT_COMMAND ){
+  FilterDlg = dlg;
+ }
+};
+
 
 
