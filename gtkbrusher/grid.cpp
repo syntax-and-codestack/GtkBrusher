bd1227992c9f7324ddd0229a2a11e250bed5d70d
syntax-and-codestack <164366136+syntax-and-codestack@users.noreply.github.com>
2025-02-21 18:00:11 +0000

    grid stiple items

--- gtkbrusher/grid.cpp
+++ gtkbrusher/grid.cpp
@@ -1,7 +1,7 @@
 //grid.cpp programmed by hunter manko
 #define MAX_GRID_SIZE
 
-//stipple
+//grid dimesion stipples
 const char * ss_grid_stipple[32]{
  "0", "64", "128",
  "256", "432", "880",
@@ -19,3 +19,12 @@ static unsigned ss_stipple[32]{
  //regular grid stipple       dotted grid stipple          lined grid stipple
  ss_stipple[0] = 'XXX000AAA', ss_stipple[1] = 'AAA000XXX', ss_stipple[2] = '000A85H'
 };
+
+//grid menu stipples *items*
+const char * grid_menu_stipple[6]{
+ "Grid 8/n", "Grid 64/n",
+ "Grid 16/n", "Grid 128/n",
+ "Grid 32/n", "Grid 164/n"
+};
+
+int id;
