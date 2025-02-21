47b160c3efeefacb4b00a6421be2275ccc391a3e
syntax-and-codestack <164366136+syntax-and-codestack@users.noreply.github.com>
2025-02-21 17:46:41 +0000

    grid stipple X000A85H

--- gtkbrusher/grid.cpp
+++ gtkbrusher/grid.cpp
@@ -4,11 +4,18 @@
 //stipple
 const char * ss_grid_stipple[32]{
  "0", "64", "128",
- "-128", "-64", "0",
- "1024", "-1024", "560",
- "-560", "2048", "-2048"
+ "256", "432", "880",
+ "1024", "2056", "3028",
+ "0", "-64", "-128",
+ "-256", "-432", "-880",
+ "-1024", "-2056", "-3028"
 };
 
 const char * ss_grid_stipple_print[3]{
  "x::%f", "y::%f", "z::%f"
+};
+
+static unsigned ss_stipple[32]{
+ //regular grid stipple       dotted grid stipple          lined grid stipple
+ ss_stipple[0] = 'XXX000AAA', ss_stipple[1] = 'AAA000XXX', ss_stipple[2] = '000A85H'
 };
